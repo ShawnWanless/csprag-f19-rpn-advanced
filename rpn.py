2a6fbadddfb6500afdd3352e6b6037ddc8fad1b6
Shawn Wanless <wanless@0587429624.wireless.umich.net>
2019-11-05 13:04:30 -0500

    stylize

--- rpn.py
+++ rpn.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import operator
+import colored
+from colored import stylize
 
 
 operators = {
@@ -9,6 +11,23 @@ operators = {
     '*': operator.mul,
     '/': operator.truediv,
 }
+
+def print_list(stack):
+    print("[", end="")
+    for item in stack[:-1]:
+        print(style(item), end=", ")
+    if stack != []:
+        print(style(stack[-1]), end="")
+    print("]")
+
+def style(val):
+    try:
+        val = int(val)
+        return stylize(val, colored.fg("green" if val >= 0 else "red"))
+    except:
+        if val in operators:
+            return stylize(val, colored.fg("orange_3"))
+        return val
 
 def calculate(myarg):
     stack = list()
@@ -22,12 +41,18 @@ def calculate(myarg):
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-        print(stack)
+        print_list(stack)
+    # if len(stack) != 1:
+    #     raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-        result = calculate(input("rpn calc> "))
+        equation = input("rpn calc> ")
+        print("input:", end=" ")
+        print_list(equation.split())
+        result = calculate(equation)
+        result = stylize(result, colored.fg("green" if result >= 0 else "red"))
         print("Result: ", result)
 
 if __name__ == '__main__':
